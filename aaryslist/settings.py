ba6125993004102144e6e7095d7b85a4bd7084c6
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-02-08 09:12:55 +0530

    Commit\

--- aaryslist/settings.py
+++ aaryslist/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+
+django_heroku.settings(locals())
